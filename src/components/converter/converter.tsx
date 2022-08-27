a4f4499af818060be615000395192b2fdef0d9ed
SlavaPachevsky <savaukr17101980@gmail.com>
2022-08-27 07:50:48 +0300

    fetch exchange rate in useEffect

--- src/components/converter/converter.tsx
+++ src/components/converter/converter.tsx
@@ -1,14 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Currency from "../currency/currency.tsx";
 import { UAH } from "../currency/currency.const.ts";
 import Today from "../today/today.tsx";
+
+import { getExchRate } from "../../helpers/fetchRate";
+import { FetchExchRate } from "./converter.types";
 
 import styles from "./converter.module.css";
 
 function Converter() {
   const [currency, setCurrency] = useState<string>(UAH);
   const [amount, setAmout] = useState<number>(0);
+  const [exchRate, setExchRate] = useState<FetchExchRate[] | null>(null);
+  const [err, setErr] = useState<string | null>(null);
+
+  useEffect(() => {
+    try {
+      getExchRate(setExchRate);
+    } catch (err) {}
+  }, []);
 
   return (
     <div className={styles.converterWrapper}>
